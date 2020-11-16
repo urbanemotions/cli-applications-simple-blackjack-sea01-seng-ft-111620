cb5b3d2494e893dc899980e9f443e79d03f90c61
Sin Wildy <sw.wildy@gmail.com>
2020-11-16 05:14:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,56 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..10)
 end
 
 def display_card_total
   # code #display_card_total here
+  puts "Your cards add up to #{num}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp 
 end
 
 def end_game
   # code #end_game here
+  puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  num = deal_card + deal_card
+  display_card_total(num)
+  num 
 end
 
 def hit?
   # code hit? here
+  prompt_user
+  input = get_user_input
+  if input == 's'
+    num
+  elsif input == 'h'
+    num += deal_card
+    num
+  else
+    
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
